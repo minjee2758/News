e5a9dbc9eca0f4208065f5ded1a08d19bde6fc0e
devgogogogo <imleo322@gmail.com>
2025-04-07 20:52:07 +0900

    [feat] 회원가입 기능 구현

--- src/main/java/com/example/news/entity/User.java
+++ src/main/java/com/example/news/entity/User.java
@@ -1,12 +1,18 @@
 package com.example.news.entity;
 
-import java.time.LocalDateTime;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @Entity
-public class User {
+@Table(name = "user")
+@AllArgsConstructor
+public class User extends BaseEntity {
 
     @Id //PK
     @GeneratedValue(strategy = GenerationType.IDENTITY) //AUTO_INCREMNET
@@ -16,23 +22,24 @@ public class User {
     private String username;
 
     @Column(nullable = false, unique = true)
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,20}$", message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 공백일 수 없습니다")
     private String email;
 
+    @Size(min = 4, max = 4)
     private String mbti;
 
     @Column(nullable = false)
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$", message = "비밀번호는 4~10자, 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
 
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
-
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
-
-    @Column(name = "deleted_at")
-    private LocalDateTime deletedAt;
-
     public User() {
-        this.createdAt = LocalDateTime.now();
+    }
+
+    public User(String username, String email, String mbti, String password) {
+        this.username = username;
+        this.email = email;
+        this.mbti = mbti;
+        this.password = password;
     }
 }
