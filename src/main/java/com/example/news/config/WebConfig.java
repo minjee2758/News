68953cb679b058eea55cb82d40193558dc50d038
minjee2758 <minjee2758@naver.com>
2025-04-09 10:42:55 +0900

    [feat] 로그인 필터 기능 추가

---
+++ src/main/java/com/example/news/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.news.config;
+
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean<Filter> loginFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+
+        filterRegistrationBean.setFilter(new LoginFilter()); // 필터 등록
+        filterRegistrationBean.setOrder(1); //순서1
+        filterRegistrationBean.addUrlPatterns("/*"); // 전체 URL에 필터 적용
+
+        return filterRegistrationBean;
+    }
+
+}
