d9339ff523cefacffda0d027ba3f228798546990
minjee2758 <minjee2758@naver.com>
2025-04-10 14:16:36 +0900

    [fix] 로그인 불가 시간 계산 오류 수정

--- src/main/java/com/example/news/service/LoginService.java
+++ src/main/java/com/example/news/service/LoginService.java
@@ -7,6 +7,7 @@ import com.example.news.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -15,6 +16,7 @@ import org.springframework.web.server.ResponseStatusException;
 * 세션 남기기, 로그인 횟수제한, 만료시간 지정의 모든 로그인 로직을 담당하는 클래스
 * 단일책임원칙에 따르도록 함!!
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -23,6 +25,7 @@ public class LoginService {
     private final PasswordEncoder passwordEncoder;
 
     public UserResponseDto login(String email, String password, HttpSession session) {
+        loginBlock(session); //로그인 로직이 실행되기 전에, 블락여부 판단하기
         User user = userRepository.findUserByEmailOrElseThrow(email);
         withdrawUser(email);
         if (passwordEncoder.matches(password, user.getPassword())) {
@@ -48,14 +51,38 @@ public class LoginService {
         if (failCount == null) {
             failCount = 0;
         }
+
         failCount++;
+        log.info(failCount+ "번째 로그인 실패");
+
         session.setAttribute("loginFailCount", failCount);
+        if (failCount == 3) {
+            // 차단 시간 시작 지점 저장 (딱 3번째 실패 시점에)
+            session.setAttribute("loginFailTime", System.currentTimeMillis());
+        }
     }
+
+    /*
+    * "현재 시간 - 로그인 실패 시점 < 차단 시간 " 이면 현재 차단되어있는 상태이므로 실패 반환
+    * 그게 아니라면 실패 횟수와 실패 시점 초기화 -> 로그인 시도할 수 있게
+     */
 
     public void loginBlock(HttpSession session) {
         Integer failCount = (Integer) session.getAttribute("loginFailCount");
-        if (failCount >5) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        Long failTime = (Long) session.getAttribute("loginFailTime");
+        if (failCount == null) { failCount =0;}
+
+        if (failCount ==null ||failCount<3) return;
+        long blockTime = 10 * 1000; //10초
+        long currentTime = System.currentTimeMillis();
+
+        if (failTime !=null&& (currentTime-failTime)< blockTime) {
+            long remainTime = (blockTime - (currentTime-failTime)) / 1000;
+            log.info(remainTime+"초 동안은 로그인할 수 없습니다");
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
         }
+        //로그인 정상적으로 이루어지면 초기화해주기
+        session.setAttribute("loginFailCount", 0);
+        session.removeAttribute("loginFailTime");
     }
 }
