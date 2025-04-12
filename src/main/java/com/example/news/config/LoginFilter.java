44c8d3c90317a06c10b88590e7517e1e658d44d1
minjee2758 <minjee2758@naver.com>
2025-04-12 19:13:22 +0900

    refactor : 로그인 필터에서 직접 에러처리

--- src/main/java/com/example/news/config/LoginFilter.java
+++ src/main/java/com/example/news/config/LoginFilter.java
@@ -1,10 +1,13 @@
 package com.example.news.config;
 
 
+import com.example.news.common.CommonResponse;
 import com.example.news.exception.CustomException;
 import com.example.news.exception.FailCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -22,18 +25,29 @@ public class LoginFilter implements Filter {
             throws IOException, ServletException {
 
         HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
         String requestURI = httpRequest.getRequestURI();
 
         log.info("로그인 필터 로직 실행");
+        HttpSession session = httpRequest.getSession(false);
 
-        if (!isWhiteList(requestURI)){
-            HttpSession session = httpRequest.getSession(false);
-            if (session == null || session.getAttribute("loginUser") == null){
-                throw new CustomException(FailCode.LOGIN_REQUIRED);
+        try {
+            if (!isWhiteList(requestURI) && (session == null || session.getAttribute("loginUser") == null)){
+                throw new RuntimeException("로그인이 필요합니다");
             }
+            FilterChain chain = filterChain;
+            chain.doFilter(servletRequest, servletResponse);
+        } catch (RuntimeException e) {
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401 Unauthorized
+            httpResponse.setContentType("application/json;charset=UTF-8");
+
+            CommonResponse<Object> errorResponse = CommonResponse.from(FailCode.LOGIN_REQUIRED, null);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(errorResponse);
+
+            httpResponse.getWriter().write(json);
         }
-        FilterChain chain = filterChain;
-        chain.doFilter(servletRequest, servletResponse);
 
     }
 
